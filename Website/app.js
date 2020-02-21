20888f29edf33afbb16e987b64a2db5b20de9fd5
RyanVoigt <ryanvoigt.0618@gmail.com>
2020-02-20 17:56:12 -0800

    update request numbers

--- Website/app.js
+++ Website/app.js
@@ -83,7 +83,7 @@ app.get('/callback', function(req, res) {
             refresh_token = body.refresh_token;
 
         var options = {
-          url: 'https://api.spotify.com/v1/me/top/tracks?limit=100',
+          url: 'https://api.spotify.com/v1/me/top/tracks?limit=30',
           headers: { 'Authorization': 'Bearer ' + access_token },
           json: true
         };
